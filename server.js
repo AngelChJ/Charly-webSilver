1a9b2f6c353151d848646f1a21610357c82feb11
AngelChJ <angelchope39@gmail.com>
2026-05-09 13:08:04 -0600

    fix: forzar puerto 8080 en Express

--- server.js
+++ server.js
@@ -4,15 +4,14 @@ import { dirname, join } from 'path';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 8080;
 
 app.use(express.static(join(__dirname, 'dist')));
 
-// Express 5: usar /{*path} en lugar de *
 app.get('/{*path}', (req, res) => {
     res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on http://0.0.0.0:${port}`);
 });
